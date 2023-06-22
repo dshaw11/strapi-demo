a0588f61e206c3cd3e2253f64fdcacacb0878e56
David Shaw <dshaw11@utk.edu>
2023-06-22 10:42:29 -0400

    added category page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 // Pages
 import Homepage from "./pages/Homepage";
 import ReviewDetails from "./pages/ReviewDetails";
+import Category from "./pages/Category";
 
 
 // Components
@@ -12,19 +13,18 @@ import Header from "./components/Header/Header";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
+    <Router className='App'>
+      {/* <div className="App"> */}
         <Header />
 
 
         <Routes>
-          <Route exact path='/' Component={Homepage}>
-          </Route>
-          <Route path='/details/:id' Component={ReviewDetails}>
-          </Route>
+          <Route exact path='/' Component={Homepage} />
+          <Route path='/details/:id' Component={ReviewDetails} />
+          <Route path='/category/:id' Component={Category} />
         </Routes>
 
-      </div>
+      {/* </div> */}
     </Router>
   );
 }
